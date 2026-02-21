6f9276676f41caa31334f1cecc0138233dc1fd71
SamuelPinaLopes <samuelpinalopesws@gmail.com>
2026-02-21 00:50:32 -0100

    I got the text, the words to change and the flags

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 how to do it?
 - get the flags to do specific things in the program (done)
 
-- get the words you want to change as a parameter
+- get the words you want to change as a parameter (done)
 
 - get the new file with a text you want to change some specific
-words
+words (get the words you wanto to change, that words should be between parentheses)
 
 - with the words you want to change, fetch for them in the text
 
@@ -19,13 +19,28 @@ place
 
 
 import sys
+import os
 
-list_flags = list()
-list_flags = sys.argv[0:]
+sys.argv = sys.argv[1:] # taking off the program name
+list_flags = list() # creating the list that will have all the flags
+list_words = list() # saving the words to change
+text_paths = list() # creating a list to save the path files
 
-for each in list_flags:
-    if '-' in each:
-        print(f"this thing is a flag: {each}\n")
+for each in sys.argv: # catching the flags 
+    if '-' == each[0]:
+        list_flags.append(each)
 
 
-print(f"this is the things you pass to the program:\n{sys.argv}")
+for text in sys.argv: # catching the files (in paths)
+    if os.path.isfile(text) == True:
+        text_paths.append(text)
+
+""" catching the last thing that should be a list of words """
+list_words = sys.argv[-1].split() # catching the last sentence passed and spliting the words in the string passed
+
+print("list of words")
+print(list_words)
+print("text to change the words on it")
+print(text_paths)
+print("list of flags passed")
+print(list_flags)
